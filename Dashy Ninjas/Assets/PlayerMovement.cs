421b0a365b8a6af056adbea6d8f00e01677a9b60
Abdullah Mortada <abdullahmortada@gmail.com>
2018-09-28 00:10:08 +0200

    Basic Mechanics

---
+++ Dashy Ninjas/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public CharacterController2D controller;
+    public float moveSpeed;
+    float hMove;
+    bool jump;
+    bool crouch;
+
+    private void Update()
+    {
+        hMove = Input.GetAxisRaw("Horizontal")*moveSpeed;
+        if(Input.GetButton("Jump"))
+        {
+            jump = true;
+        }
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(hMove * Time.fixedDeltaTime,crouch,jump);
+        jump = false;
+    }
+}
---
+++ Dashy Ninjas/Assets/Slice.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slice : MonoBehaviour {
+    Vector2 mouseLook;
+    public float moveSpeed = 8;
+    public LineRenderer line;
+    public int maxUses = 3;
+    int uses;
+    CharacterController2D controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController2D>();
+    }
+    void Update () {
+        if (controller.m_Grounded)
+        {
+            uses = 0;
+        }
+        mouseLook = Input.mousePosition;
+        mouseLook = Camera.main.ScreenToWorldPoint(mouseLook);
+        if (Input.GetButtonDown("Fire1") && uses<maxUses)
+        {
+            StartCoroutine(Slash(moveSpeed));
+        }
+        Debug.Log(mouseLook);
+	}
+    IEnumerator Slash(float speed)
+    {
+        line.enabled = true;
+        line.SetPosition(0, transform.position);
+        transform.position = Vector2.Lerp(transform.position, mouseLook, speed);
+        line.SetPosition(1, transform.position);
+        yield return new WaitForSeconds(0.2f);
+        line.enabled = false;
+        uses += 1;
+    }
+}
