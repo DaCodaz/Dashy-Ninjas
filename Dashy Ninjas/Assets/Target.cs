54a97b0dd5f69145012fdf00118d5d48e1e01db1
Abdullah Mortada <abdullahmortada@gmail.com>
2018-11-24 20:13:49 +0200

    Fixed Target Script

--- Dashy Ninjas/Assets/Target.cs
+++ Dashy Ninjas/Assets/Target.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour {
-    public GameObject player;
+    public GameObject line;
     EdgeCollider2D lineCol;
     // Use this for initialization
     void Start () {
-        lineCol = player.GetComponentInChildren<EdgeCollider2D>();
+        lineCol = line.GetComponent<EdgeCollider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float dist = Vector2.Distance(gameObject.transform.position, lineCol.transform.position);
-        if (dist == 0) takeDamage();
+        if (dist < 1) takeDamage();
 	}
     void takeDamage()
     {
