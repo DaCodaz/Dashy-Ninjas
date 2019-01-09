9ecc4b373226f991be56e1a3456feb0f4166c123
Abdullah Mortada <abdullahmortada@gmail.com>
2019-01-09 14:31:11 +0200

    volume

---
+++ Dashy Ninjas/Assets/Scripts/optionsMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class optionsMenu : MonoBehaviour {
+    public AudioMixer audioMixer;
+
+    public void SetAudio(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+}
